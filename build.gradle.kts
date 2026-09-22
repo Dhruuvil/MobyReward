63d8125181190be17fc11fbdc153c11644cc73e0
Dhruvil <dhruvilthesiya14@gmail.com>
2026-09-22 11:29:18 +0530

    Fix Maven publication for JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
     alias(libs.plugins.android.library)
     `maven-publish`
@@ -17,6 +19,32 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = providers.gradleProperty("group")
+                    .orElse("com.github.Dhruuvil")
+                    .get()
+
+                artifactId = "MobyReward"
+
+                version = providers.gradleProperty("version")
+                    .orElse("1.0.1")
+                    .get()
+            }
+        }
     }
 }
 
